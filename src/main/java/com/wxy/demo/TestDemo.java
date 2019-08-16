5d587f10cd71faf1d0b2ae91b060cba89d75d89c
wxy <wangxiaoyuan@pand-auto.com>
2019-08-16 17:54:31 +0800

    test

---
+++ src/main/java/com/wxy/demo/TestDemo.java
@@ -0,0 +1,14 @@
+package com.wxy.demo;
+
+/**
+ * @Author wxy
+ * @Date 19-8-16 下午5:52
+ * @Description TODO
+ **/
+public class TestDemo {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = a >>> 2;
+        System.out.println(b);
+    }
+}
