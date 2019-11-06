07f3b50bb67aeb355ce9a5d121e13880480855c6
wangxiaoyuan <243548880@qq.com>
2019-11-06 11:22:00 +0800

    decimal

--- src/main/java/com/wxy/demo/math/Demo1.java
+++ src/main/java/com/wxy/demo/math/Demo1.java
@@ -8,7 +8,7 @@ package com.wxy.demo.math;
 public class Demo1 {
     public static void main(String[] args) {
         double a = 1e4;
-        double b = 1e4d;
+        double b = 5e4d;
         System.out.println(a);
         System.out.println(b);
     }
