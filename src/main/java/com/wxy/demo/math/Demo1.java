664f5b48450df095f502035c53e0dc4a5d854fbe
wangxiaoyuan <243548880@qq.com>
2019-11-06 11:19:29 +0800

    decimal

---
+++ src/main/java/com/wxy/demo/math/Demo1.java
@@ -0,0 +1,15 @@
+package com.wxy.demo.math;
+
+/**
+ * @Author wangxiaoyuan
+ * @Date 19-11-6 上午11:17
+ * @Description TODO
+ **/
+public class Demo1 {
+    public static void main(String[] args) {
+        double a = 1e4;
+        double b = 1e4d;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
